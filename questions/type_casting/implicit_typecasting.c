345c7de2e8aea055d3a3946afe58d3f669111db4
PatrickAJoseph <patrickajoseph.projects@gmail.com>
2026-01-17 21:50:09 +0530

    Added implicit_typecasting.c

---
+++ questions/type_casting/implicit_typecasting.c
@@ -0,0 +1,46 @@
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int a1 = 15;
+    float b1 = -17.5;
+
+    //
+    // What will be the output of thie program ?
+    //  
+    // Your answer:
+    //
+    //
+    // Your explanation (mandatory):
+    //
+    //
+    //
+    //
+
+    printf("Output of a1 + b1 = %f\n", a1+b1);
+
+    int a2 = 100;
+    float b2 = 12.57f;
+    int c2;
+
+    c2 = a2 + b2;
+
+    //
+    // What will be the output of thie program ?
+    //  
+    // Your answer:
+    //
+    //
+    // Your explanation (mandatory):
+    //
+    //
+    //
+    //
+
+    printf("Output of a2 + b2 = %f\n", c2);
+
+    return 0;
+}
